209378f8d31fa6c267d96fa748abec2d518459f3
Usamoi Minakami <usamoi@outlook.com>
2021-11-04 14:27:41 +0800

    Add Successive Shortest Path (Dijkstra version)

--- template_dijkstra.cpp
+++ template_dijkstra.cpp
@@ -15,6 +15,7 @@ struct Dijkstra {
         std::vector<int> e;
         ll d, h = 0;
         int prev;
+        int inq;
     };
     std::vector<Vertex> V;
     std::vector<Edge> E;
@@ -28,6 +29,30 @@ struct Dijkstra {
     }
     ll cost(const Edge &e) const {
         return e.cost + V[e.u].h - V[e.v].h;
+    }
+    bool bellman(int s, int t) {
+        for (auto &u : V) {
+            u.d = INF, u.inq = false;
+        }
+        std::queue<int> q;
+        V[s].prev = -1, V[s].d = 0;
+        V[s].inq = true, q.push(s);
+        while (!q.empty()) {
+            int u = q.front();
+            V[u].inq = false, q.pop();
+            for (auto idx : V[u].e) {
+                Edge &e = E[idx];
+                if (e.cap == e.flow)
+                    continue;
+                if (V[e.v].d > V[u].d + e.cost) {
+                    V[e.v].prev = idx, V[e.v].d = V[u].d + e.cost;
+                    if (!V[e.v].inq) {
+                        V[e.v].inq = true, q.push(e.v);
+                    }
+                }
+            }
+        }
+        return V[t].d != INF;
     }
     bool dijkstra(int s, int t) {
         std::priority_queue<std::pair<ll, int>, std::vector<std::pair<ll, int>>, std::greater<std::pair<ll, int>>> q;
@@ -50,6 +75,25 @@ struct Dijkstra {
             }
         }
         return V[t].d != INF;
+    }
+    void successive_shortest_path(int s, int t, ll &out_flow, ll &out_cost) {
+        int n = V.size() - 1;
+        int m = E.size() / 2;
+        out_flow = 0;
+        for (bellman(s, t); dijkstra(s, t);) {
+            for (int u = 0; u <= n; u++) {
+                V[u].h += V[u].d;
+            }
+            ll flow = INF;
+            for (int x = V[t].prev; x != -1; x = V[E[x].u].prev)
+                flow = std::min(flow, E[x].cap - E[x].flow);
+            for (int x = V[t].prev; x != -1; x = V[E[x].u].prev)
+                E[x].flow += flow, E[x ^ 1].flow -= flow;
+            out_flow += flow;
+        }
+        out_cost = 0;
+        for (int i = 0; i < m; i++)
+            out_cost += E[i << 1].flow * E[i << 1].cost;
     }
     void capacity_scaling(int s, int t, ll &out_flow, ll &out_cost) {
         int n = V.size() - 1;
@@ -116,9 +160,9 @@ int main() {
         scanf("%d%d%lld%lld", &u, &v, &cap, &cost);
         dijkstra.addEdge(u, v, cap, cost);
     }
-    s = 1, t = n;
     long long out_flow, out_cost;
-    dijkstra.capacity_scaling(s, t, out_flow, out_cost);
+    s = 1, t = n;
+    dijkstra.successive_shortest_path(s, t, out_flow, out_cost);
     printf("%lld %lld\n", out_flow, out_cost);
     return 0;
 }
