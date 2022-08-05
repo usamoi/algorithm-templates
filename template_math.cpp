42646828049a56707234344073bbf4572700cf89
Usamoi <usamoi@outlook.com>
2022-08-05 16:04:38 +0800

    Add Xor Basis

--- template_math.cpp
+++ template_math.cpp
@@ -1,15 +1,20 @@
 #include <algorithm>
 #include <array>
 #include <cmath>
+#include <iostream>
+#include <numeric>
+#include <optional>
+#include <vector>
 
 template <const int N>
 struct Gauss {
     using Vector = std::array<double, N>;
-    static constexpr double eps = 1e-9;
+    static constexpr double EPS = 1e-9;
 
     std::array<Vector, N> A;
     Vector B;
     Vector X;
+
     bool gauss() {
         std::array<int, N> s;
         std::fill(s.begin(), s.end(), -1);
@@ -18,7 +23,7 @@ struct Gauss {
             for (int j = 0; j < N; j++)
                 if (s[j] == -1 && ((f == -1) || std::fabs(A[f][i]) < std::fabs(A[j][i])))
                     f = j;
-            if (f == -1 || std::fabs(A[f][i]) < eps)
+            if (f == -1 || std::fabs(A[f][i]) < EPS)
                 return false;
             s[f] = i;
             for (int j = 0; j < N; j++)
@@ -32,6 +37,55 @@ struct Gauss {
         for (int i = 0; i < N; i++)
             X[s[i]] = B[i] / A[i][s[i]];
         return true;
+    }
+};
+
+template <typename T = uint64_t>
+struct Basis {
+    static constexpr int BITS = sizeof(T) * 8;
+
+    std::array<T, BITS> p = {};
+    int s = 0;
+    bool z = false;
+
+    void insert(T x) {
+        for (int i = 0; i < BITS; i++) {
+            if ((x >> i) & 1) {
+                if (p[i]) {
+                    x ^= p[i];
+                }
+            }
+        }
+        if (x) {
+            int j = std::bit_width(x) - 1;
+            for (int k = j + 1; k < BITS; k++) {
+                if ((p[k] >> j) & 1) {
+                    p[k] ^= x;
+                }
+            }
+            p[j] = x, s++;
+        } else {
+            z = true;
+        }
+    }
+
+    std::optional<T> query(T k) {
+        if (z) {
+            k--;
+        }
+        T ans = 0;
+        if (k >> s) {
+            return std::nullopt;
+        }
+        for (int i = 0; i < BITS; i++) {
+            if (p[i]) {
+                if (k & 1) {
+                    ans ^= p[i];
+                }
+                k >>= 1;
+            }
+        }
+        return ans;
     }
 };
 
